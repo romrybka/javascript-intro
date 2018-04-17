5c1c1f951c26f707b315e83417ce6df0b40dbd0b
romrybka <rprybka@gmail.com>
2018-04-17 13:57:59 +0300

    fixed task 2.8

--- hard/32.js
+++ hard/32.js
@@ -26,3 +26,31 @@ function validate(key, obj) {
 
 validate(["name","surname"], {name:"Roman",surname:"Rodomansky"});
 validate(["name","surname","age"], {name:"Roman",surname:"Rodomansky"});
+
+/*
+function validate(arr, person)
+{
+    var result = {};
+    for(var i=0; i < arr.length; i++)
+    {
+        if (!(arr[i] in person)){
+            result[arr[i]] = 1;
+        }else{
+            result[arr[i]] += 1;
+        }
+    }
+    for (var prop in result) {
+        if (result[prop] == 1) {
+            throw prop;
+        }
+    }
+    return true;
+};
+
+try{
+    validate(['name', 'surname', 'age'],{name:"Roman",surname:"Rodomansky", age: "10"});
+    validate(['name', 'surname', 'age'],{name:"Roman",surname:"Rodomansky"});
+}catch (e) {
+    console.log(e, "wasn't passed on object");
+}
+*/
--- medium/23.js
+++ medium/23.js
@@ -1,3 +1,33 @@
 // 2.3) Write a JavaScript function that generates all combinations of a string. For i.e. input dog, should respond with d,do,dog,o,og,g.
 
 
+/*
+var arr = [];
+for (var i = 0; i <= 100; i++){
+    arr[i] = i;
+}
+
+arr.sort(function (a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+});
+
+function binary_Search(arr, i) {
+    var mid = Math.floor(arr.length / 2);
+    console.log(arr[mid], i);
+
+    if (arr[mid] === i) {
+        console.log('finish', arr[mid], i);
+        return arr[mid];
+    } else if (arr[mid] < i && arr.length  > 1) {
+        return binary_Search(arr.splice(mid, Number.MAX_VALUE), i);
+    } else if (arr[mid] > i && arr.length  > 1) {
+        return binary_Search(arr.splice(0, mid), i);
+    } else {
+        console.log('not here', i);
+        return -1;
+    }
+
+}
+var result = binary_Search(arr, 27);
+*/
